42a45ccfcaedf99d15ef0cbd408710875de408e2
vvseva <wirzet1@gmail.com>
2024-11-13 08:41:38 -0600

    v1.1 chat

--- study2/client/src/intro-exit/Introduction.jsx
+++ study2/client/src/intro-exit/Introduction.jsx
@@ -9,9 +9,15 @@ export function Introduction({ next }) {
       </h3>
       <div className="mt-2 mb-6">
         <p className="text-sm text-gray-500">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius aliquam
-          laudantium explicabo pariatur iste dolorem animi vitae error totam. At
-          sapiente aliquam accusamus facere veritatis.
+            <div className="prose lg:prose-xl"> {/* Added prose class for better typography */}
+                You are an accomplished Astrobiologist
+                <br></br>
+                Called to join a secret, high-priority, elite Planetary Science Team
+                <br></br>
+                Your Astrobiology Team needs to determine humanity’s “plan B”
+                <br></br>
+                Which of three planets presents the best chance of supporting a human settlement?
+                <div/>
         </p>
       </div>
       <Button handleClick={next} autoFocus>
